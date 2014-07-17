248cacd90a0b8d68289d694afc10586286692899
Yutaka Sekine <s1313101@u.tsukuba.ac.jp>
2014-07-17 19:13:12 +0900

    手順ページからレシピ詳細ページへの遷移を作成、全ページにホームへ戻るを追加

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -3,5 +3,7 @@ class ProceduresController < ApplicationController
   def show
     @recipe = Recipe.find(params[:recipe_id])
     @procedure = Procedure.find(params[:id])
+    @procedures = Procedure.where(recipe_id: params[:recipe_id])
   end
+
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,7 +1,5 @@
 class Procedure < ActiveRecord::Base
+
   belongs_to :recipe
 
-  def show
-    @procedure = Procedure.find(params[:id])
-  end
 end
