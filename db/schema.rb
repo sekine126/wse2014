ea6cafc745a28ce399c6c4b5af913d3fbaa2e6e0
Yutaka Sekine <llik.iris@gmail.com>
2014-10-06 17:28:43 +0900

    さっきの続き

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817161039) do
+ActiveRecord::Schema.define(version: 20141006082320) do
 
   create_table "foods", force: true do |t|
     t.string   "name"
     t.string   "category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "ingredients", force: true do |t|
-    t.integer  "ingredient_id"
-    t.string   "name"
-    t.string   "genre"
-    t.decimal  "amount"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
