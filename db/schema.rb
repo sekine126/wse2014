25592a153054fbaa9343e8f56b762003f68dd5bb
Yutaka Sekine <llik.iris@gmail.com>
2014-10-09 03:30:53 +0900

    update migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006082320) do
+ActiveRecord::Schema.define(version: 20140817161039) do
 
   create_table "foods", force: true do |t|
     t.string   "name"
