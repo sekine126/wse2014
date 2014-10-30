2787cb5e327f158086237e91e47bfdafb9f56b84
hyphen02 <hyphen.com.02@gmail.com>
2014-10-30 15:45:31 +0900

    食材データの入力，とりあえずのエラー処理

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -15,6 +15,24 @@ class ProceduresController < ApplicationController
   	@foods = Food.where(id: params[:food_id])
   	@values= params[:value]
   	@procedures = params[:procedure]
+    
+
+    if params[:from_value]
+      @values.each do |value|
+        # valueからの遷移時エラー処理
+        if value[:val].blank?
+          render :text => "<h1>value ERROR</h1>"
+        end
+      end
+    else
+      @procedures.each do |procedure|
+        # 手順追加時のエラー処理
+        if procedure[:text].blank?
+          render :text => "<h1>procedure ERROR</h1>"
+        end
+      end
+    end    
+
   end
 
 end
--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,5 +1,9 @@
 class ValuesController < ApplicationController
 	def new
+		if params[:select].blank?
+			render :text => "<h1>ERROR</h1>"
+		end
+
 		@foods = Food.where(id: params[:select])
 	end
 
