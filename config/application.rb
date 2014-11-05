3bb4303780c37d7caf3745c85dcaf7e43b9ddce9
Yutaka Sekine <llik.iris@gmail.com>
2014-11-05 18:24:21 +0900

    文字コードをUTF8に明示的に指定

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,17 @@ module Wse2014
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
+
+  end
+  
+  class ApplicationController < ActionController::Base
+    # 日本語化処理
+    before_filter :set_charset
+
+    private
+    def set_charset
+      headers["Content-Type"] = "text/html; charset=UTF-8"
+    end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# 文字コードをUTF-8に指定
+$KCODE = "UTF8"
