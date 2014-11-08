fb4e1c53e3fa8c8ef14a41595061c16e653268e6
SeijuOda <alternating_c@yahoo.co.jp>
2014-11-08 17:48:17 +0900

    17:48

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+# coding:utf-8
 class RecipesController < ApplicationController
 
   def index
@@ -13,11 +14,49 @@ class RecipesController < ApplicationController
     @foods = Food.where(id: params[:food_id])
     @values = params[:value]
     @procedures = params[:procedure]
+    @recipe=Recipe.new
 
     if @procedures.blank?
       flash[:error] = "手順を入力してください03"
       redirect_to :controller=>"procedures", :action=>"new", :procedure => params[:procedure], :value => params[:value], :food_id => params[:food_id], :from_value=>true
     end
   end
+
+  def create
+    @recipe=Recipe.new(recipe_params)
+    @procedure=Procedure.new(procedure_params)
+    @value=Value.new(value_params)
+
+    respond_to do |format|
+      if @recipe.save && @procedure.save && @value.save
+        format.html { redirect_to @recipe }
+      end
+    end
+  end
+
+  private
+    def recipe_params
+      params[:recipe].permit(
+        :name
+        )
+    end
+
+    def procedure_params
+      params.require(:procedure).permit(
+        :number,
+        :detail,
+        :sec,
+        :play,
+        :recipe_id
+        )
+    end
+
+    def value_params
+      params[:value].permit(
+        :food_id,
+        :recipe_id,
+        :value
+        )
+    end
 
 end
--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,3 +1,4 @@
+# coding:utf-8
 class ValuesController < ApplicationController
 	def new
 		if params[:select].blank?
