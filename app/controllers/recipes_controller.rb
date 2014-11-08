bef3eea781125c95eb2d171af2bacf48023ee622
Yutaka Sekine <llik.iris@gmail.com>
2014-11-08 21:28:51 +0900

    レシピ保存完了画面にテンプレートを適用

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,21 +11,20 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @foods = Food.where(id: params[:food_id])
+    @recipe = Recipe.new
     @values = params[:value]
     @procedures = params[:procedure]
-    @recipe=Recipe.new
 
     if @procedures.blank?
-      flash[:error] = "手順を入力してください03"
+      flash[:error] = "手順を入力してください"
       redirect_to :controller=>"procedures", :action=>"new", :procedure => params[:procedure], :value => params[:value], :food_id => params[:food_id], :from_value=>true
     end
   end
 
   def create
-    @recipe=Recipe.new(recipe_params)
-    @procedures=Procedure.new(procedure_params)
-    @values=Value.new(value_params)
+    @recipe = Recipe.new(recipe_params)
+    @procedures = Procedure.new(params[:procedure])
+    @values = Value.new(params[:value])
 
     @procedures.recipe = @recipe
     @values.recipe = @recipe
@@ -33,31 +32,12 @@ class RecipesController < ApplicationController
     @recipe.save
     @procedures.save
     @values.save
-
   end
 
   private
     def recipe_params
       params[:recipe].permit(
         :name
-        )
-    end
-
-    def procedure_params
-      params[:recipe].permit(
-        :number,
-        :detail,
-        :sec,
-        :play,
-        :recipe_id
-        )
-    end
-
-    def value_params
-      params[:recipe].permit(
-        :food_id,
-        :recipe_id,
-        :value
         )
     end
 
